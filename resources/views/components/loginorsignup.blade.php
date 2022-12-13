5ac37160653ad90b2763f4ea309b1a8654e16318
Marwan1998 <Dragonkingofhell@gmail.com>
2022-12-13 14:23:02 +0200

    changed login and signup design

--- resources/views/components/loginorsignup.blade.php
+++ resources/views/components/loginorsignup.blade.php
@@ -8,30 +8,36 @@
       @endif
     </div>
 </nav>
-<div class="container border border-1 rounded-4 border-info w-50" style="background-color: #2148C0;">
-    <div class="row">
-        <div class="col-12 text-center ">
-            <h5 class="display-6 text-info pb-2 fw-bold">{{ ucfirst($type) }}</h5>
-            <p class="text-light bg-success">{{ session('status') }}</p>
-            <div class="w-50 justify-content-center">
-                <form action="{{ $type }}" method="POST" class="form-signup">
-                    @csrf
-                    <input type="name" class="form-control" name="name" placeholder="Username" value="{{ old('name')}}">
-                    <p class="text-danger d-inline">@error('name'){{ $message }}@enderror</p>
-                    <br>
-                    <input type="password" class="form-control" name="password" placeholder="Username">
-                    <p class="text-danger d-inline">@error('password'){{ $message }}@enderror</p>
-                    <p class="text-danger d-inline">@error('approve'){{ $message }}@enderror</p>
-                    <br>
-                    <button type="submit" class="btn btn-primary">{{ ucfirst($type) }}</button>
-                </form>
+
+<div class="container-lg">
+    <div class="row justify-content-center my-5">
+        <div class="col-lg-6 border border-1 rounded-4 border-info p-5">
+            <div class="text-center justity-content-center">
+                <h5 class="display-6 text-info pb-2 fw-bold">{{ ucfirst($type) }}</h5>
+                <p class="text-light bg-success">{{ session('status') }}</p>
             </div>
-            <br>
+            <form action="{{ $type }}" method="POST" class="form-signup">
+                @csrf
+                <p class="form-label text-danger fw-bold">@error('name'){{ '*'.$message }}@enderror</p>
+                <div class="mb-4 input-group">
+                    <input type="name" class="form-control" name="name" placeholder="Username" value="{{ old('name') }}">
+                </div>
+                <p class="form-label text-danger fw-bold">@error('password'){{ '*'.$message }}@enderror</p>
+                <div class="mb-4 mt-2 input-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                </div>
+                <p class="form-label text-danger fw-bold">@error('approve'){{ $message }}@enderror</p>
+                <div class="mb-4 text-center">
+                    <button type="submit" class="btn btn-primary">{{ ucfirst($type) }}</button>
+                </div>
+            </form>
+            <div class="text-center justity-content-center">
                 @if ($type == 'login')
                 <p class="text-info">don't have account? <a href="/signup" class="text-info text-decoration-none fw-bold">signup</a></p>
                 @else
                 <p class="text-info">Already have an account? <a href="/login" class="text-info text-decoration-none fw-bold">login</a></p>
                 @endif
+            </div>
         </div>
     </div>
 </div>
